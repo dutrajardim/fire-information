41a28cf127791934884886a0271c0396debaf385
Rafael Dutra Jardim <rafaeldjardim@gmail.com>
2022-03-13 09:19:54 -0300

    debugging s3fs hook

--- airflow/plugins/hooks/s3fs.py
+++ airflow/plugins/hooks/s3fs.py
@@ -15,7 +15,10 @@ class S3fsHook(BaseHook):
             "aws_secret_access_key": self.conn.password,
         }
 
+        self.log(self.conn.extra)
+        self.log(self.conn.extra_dejson)
         if self.conn.extra and "client_kwargs" in self.conn.extra_dejson:
             client_kwargs = {**client_kwargs, **self.conn.extra_dejson["client_kwargs"]}
 
+        self.log(client_kwargs)
         return s3fs.S3FileSystem(client_kwargs=client_kwargs)
