3bf22b8f6345be5fd00676de9cac1e047a487b2d
Rafael Dutra Jardim <rafaeldjardim@gmail.com>
2022-03-12 22:45:40 -0300

    creating the airflow folder

--- airflow/plugins/__init__.py
+++ airflow/plugins/__init__.py
@@ -1,10 +1,12 @@
+from __future__ import division, absolute_import, print_function
+
 from airflow.plugins_manager import AirflowPlugin
 
 import operators
 
 # Defining the plugin class
-class UdacityPlugin(AirflowPlugin):
-    name = "udacity_plugin"
+class DutraPlugin(AirflowPlugin):
+    name = "dutra_plugin"
     operators = [
         operators.DataQualityOperator,
     ]
